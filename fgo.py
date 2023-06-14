afe481dab46266800c4f0cb0a6ff06a8147c5269
ThiefInMoonlight <767373506@qq.com>
2023-06-14 22:41:17 +0800

    上传fgo脚本修改

--- fgo.py
+++ fgo.py
@@ -1,16 +1,15 @@
 # -*- encoding=utf8 -*-
-__author__ = "xuecm"
 
 import os
 import time
 
 from airtest.core.api import *
+from airtest.cli.parser import cli_setup
 
 auto_setup(__file__)
 import random
 import sys
 import atexit
-import mailSender
 
 logDir = 'C:/fgoLog/'
 map = {
@@ -36,21 +35,40 @@ map = {
     'CBA_name_pic': Template(r"tpl1610816953292.png", record_pos=(-0.153, -0.043), resolution=(1581, 889)),
     'kongming_name_pic': Template(r"tpl1610816962826.png", record_pos=(-0.111, 0.103), resolution=(1581, 889)),
     'meilin_name_pic': Template(r"tpl1610816982721.png", record_pos=(-0.164, 0.089), resolution=(1581, 889)),
+    'caber_name_pic':Template(r"Caster_Saber_Name.png", record_pos=(-0.164, 0.089), resolution=(1581, 889)),
     'reward_pic': "",
-
+    'another_ten_gacha':Template(r"AnotherTenGacha.png", record_pos=(-0.164, 0.089), resolution=(1581, 889)),
+    'gacha_confirm':Template(r"GachaConfirm.png", record_pos=(-0.164, 0.089), resolution=(1581, 889)),
 }
 
-# round1 = {"skills": [[1, 1], [1, 3], [3, 1], [3, 3]], "masterSkills": [[2, 2]], "hoguNo": 3}
-# round2 = {"skills": [], "masterSkills": [], "hoguNo": 1}
-# round3 = {"skills": [], "masterSkills": [], "hoguNo": 2}
+#5号位 随意助战，狂那， 杀狐 暗狐 奥博龙
+#roundConfig1 = {"skills": [[1, 1], [1, 2], [2, 2, 1], [2, 3, 1], [3, 1],[3, 2, 1], [3,3]], "masterSkills": [], "hoguNo": [3]}
+#roundConfig2 = {"skills": [[3, 1]], "masterSkills": [[3, 3, 4]], "hoguNo": [1]}
+#roundConfig3 = {"skills": [[2, 1, 1], [3, 2, 1]], "masterSkills": [[1]], "hoguNo": [1]}
 
-roundConfig1 = {"skills": [[2, 2], [2, 3], [1, 1], ], "masterSkills": [], "hoguNo": 1}
-roundConfig2 = {"skills": [[1, 3], [2, 1, 1], [3, 1, 1], [1, 2, 1]], "masterSkills": [[3, 2, 4]], "hoguNo": 1}
-roundConfig3 = {"skills": [[2, 1, 1], [2, 2], [2, 3, 1], [3, 2], [3, 3, 1]], "masterSkills": [], "hoguNo": 1}
+# 2号位 助战 caber, 1号陈宫
+#roundConfig1 = {"skills": [[2, 1], [2, 3, 1], [3, 2], [3, 3, 1]], "masterSkills": [], "hoguNo": [1]}
+#roundConfig2 = {"skills": [[2, 1], [2, 2, 1], [2, 3, 1], [1, 2]], "masterSkills": [], "hoguNo": [1]}
+#roundConfig3 = {"skills": [[2, 1], [2, 2, 1]], "masterSkills": [], "hoguNo": [1]}
 
-skillTimeSleep = 8
-atkTimeSleep = 5
-normalTimeSleep = 5
+# 2号位 助战 caber, 1号陈宫
+#roundConfig1 = {"skills": [[2, 1], [2, 2, 1], [2, 3, 1], [3, 1], [3, 3] ], "masterSkills": [], "hoguNo": [1]}
+#roundConfig2 = {"skills": [[2, 1], [2, 2, 1], [2, 3, 1], [3, 2, 1], [1, 2]], "masterSkills": [[3,1]], "hoguNo": [1]}
+#roundConfig3 = {"skills": [[2, 1, 1], [2, 2], [2, 3]], "masterSkills": [], "hoguNo": [1]}
+
+# 1号位 梅林助战
+#roundConfig1 = {"skills": [[2, 2], [2, 3] ], "masterSkills": [], "hoguNo": 2}
+#roundConfig2 = {"skills": [[3, 2], [3, 3]], "masterSkills": [[3, 2, 4]], "hoguNo": 3}
+#roundConfig3 = {"skills": [[1, 1], [1, 3, 2], [2, 1], [2, 3]], "masterSkills": [[1]], "hoguNo": 2}
+
+# 1号助战 caber 2号程宫 3号大公 4号奥博龙 311副本
+roundConfig1 = {"skills": [[1, 1], [1, 2, 2], [1, 3, 3], [3, 2], [3, 3]], "masterSkills": [], "hoguNo": [2]}
+roundConfig2 = {"skills": [[1, 1]], "masterSkills": [], "hoguNo": [3]}
+roundConfig3 = {"skills": [[1, 2, 3], [1, 3, 3], [3, 1]], "masterSkills": [], "hoguNo": [3]}
+
+skillTimeSleep = 5
+atkTimeSleep = 2
+normalTimeSleep = 2
 
 maxLoop = 1000
 appleUsedNum = 0
@@ -133,19 +151,16 @@ class Core:
             pass
 
     def log(self, msg, level=1):
-        print(self.logFileName)
         # level 1 debug级别 2 info级别
         log = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " " + msg + "\n"
-        if level >= 1:
-            print (log)
-        if level >= 2:
-            f = open(self.logFileName, 'a')
-            f.write(log)
-            f.close()
+        #f = open(self.logFileName, 'a')
+        #f.write(log)
+        #f.close()
+        print(log)
 
 
 core = Core()
-tempFileName = "fgo" + time.strftime("%Y-%m-%d", time.localtime()) + ".log"
+tempFileName = "fgo" + ".log"
 core.createLogFile(logDir + tempFileName)
 
 
@@ -199,6 +214,11 @@ class SceneCheck:
             core.coreSleep(1, "等待进入战斗结算界面")
             flag, position = core.existPic('battle_finish_sign')
             if flag:
+                core.coreTouch(0, 600, "战斗结算界面1")
+                core.coreTouch(0, 600, "战斗结算界面2")
+                core.coreTouch(0, 600, "战斗结算界面3")
+                core.coreTouch(0, 600, "战斗结算界面4")
+                core.coreTouch(0, 600, "战斗结算界面5")
                 return True
             flag, position = core.existPic('attack_button')
             if flag:
@@ -210,35 +230,38 @@ class SceneCheck:
 sceneCheck = SceneCheck()
 
 
-def card(hoguNum=0):
+def card(hogu=0):
     sceneCheck.waitForBattleStart()
-    core.coreTouch(1400, 740, "点击attack", 3)
+    core.coreTouch(1524, 692, "点击attack", 3)
     atkNum = 3
-    if hoguNum != 0:
-        hogutankai(hoguNum)
-        atkNum = 2
+    for i in hogu:
+        hogutankai(i)
+    
+    atkNum = 3-len(hogu)
     randomIndex = random.sample(range(1, 5), atkNum)  # 随机指令牌
-
+    
     for i in randomIndex:
         atkByPosition(i)
-    if hoguNum != 0:
-        core.log('使用{} 宝具')
+    if len(hogu) != 0:
+        core.log('使用 宝具')
     else:
         core.log('随机平A')
 
 
 def useHeroSkill(heroNo, skillNo, target=0):
-    y = 710
+    y = 680
     if heroNo == 1:
-        x = 90
+        x = 95
     elif heroNo == 2:
         x = 480
     elif heroNo == 3:
         x = 870
     x += 110 * (skillNo - 1)
-    core.coreTouch(x, y, "{}号英灵的{}技能".format(heroNo, skillNo), skillTimeSleep)
     if target != 0:
-        core.coreTouch(400 + 300 * (target - 1), 560, "对{}号英灵使用".format(target), skillTimeSleep)
+        core.coreTouch(x, y, "{}号英灵的{}技能".format(heroNo, skillNo), 3)
+        core.coreTouch(500 + 330 * (target - 1), 560, "对{}号英灵使用".format(target), skillTimeSleep)
+    else:
+        core.coreTouch(x, y, "{}号英灵的{}技能".format(heroNo, skillNo), skillTimeSleep)
 
 
 def useMasterSkill(skillNo, heroNo=0, changeHeroNo=0):
@@ -246,25 +269,25 @@ def useMasterSkill(skillNo, heroNo=0, changeHeroNo=0):
     # 910,430
     # 170,426
     #     791,768
-    core.coreTouch(1470, 380, "魔术目标奖励技能菜单", normalTimeSleep)
-    core.coreTouch(1120 + 110 * (skillNo - 1), 380, "魔术目标奖励{}技能".format(skillNo), normalTimeSleep)
+    core.coreTouch(1600, 380, "魔术目标奖励技能菜单", normalTimeSleep)
+    core.coreTouch(1260 + 110 * (skillNo - 1), 380, "魔术目标奖励{}技能".format(skillNo), normalTimeSleep)
     if changeHeroNo == 0:
         if heroNo != 0:
             core.coreTouch(400 + 300 * (heroNo - 1), 560, "对{}号英灵使用".format(heroNo), normalTimeSleep)
     else:
-        core.coreTouch(170 + 240 * (heroNo - 1), 430, "选择交换{}号英灵".format(heroNo), normalTimeSleep)
-        core.coreTouch(170 + 240 * (changeHeroNo - 1), 430, "选择交换{}号英灵".format(changeHeroNo), normalTimeSleep)
-        core.coreTouch(790, 770, "确认交换", normalTimeSleep)
+        core.coreTouch(300 + 240 * (heroNo - 1), 430, "选择交换{}号英灵".format(heroNo), normalTimeSleep)
+        core.coreTouch(300 + 240 * (changeHeroNo - 1), 430, "选择交换{}号英灵".format(changeHeroNo), normalTimeSleep)
+        core.coreTouch(800, 750, "确认交换", normalTimeSleep)
         sceneCheck.waitForBattleStart()
 
 
 def hogutankai(heroNo):
     # 1080 800 520
-    core.coreTouch(520 + 280 * (heroNo - 1), 250, "宝具使用")
+    core.coreTouch(540 + 280 * (heroNo - 1), 250, "宝具使用")
 
 
 def atkByPosition(order):
-    core.coreTouch(150 + 320 * (order - 1), 600, "指令卡{}".format(order), atkTimeSleep)
+    core.coreTouch(250 + 300 * (order - 1), 600, "指令卡{}".format(order), atkTimeSleep)
 
 
 def doOneRoundByConfig(config):
@@ -273,11 +296,6 @@ def doOneRoundByConfig(config):
     masterSkills = config["masterSkills"]
     hoguNo = config["hoguNo"]
 
-    for skill in skills:
-        if len(skill) == 3:
-            useHeroSkill(skill[0], skill[1], skill[2])
-        else:
-            useHeroSkill(skill[0], skill[1])
     for masterSkill in masterSkills:
         if len(masterSkill) == 1:
             useMasterSkill(masterSkill[0])
@@ -285,6 +303,11 @@ def doOneRoundByConfig(config):
             useMasterSkill(masterSkill[0], masterSkill[1])
         else:
             useMasterSkill(masterSkill[0], masterSkill[1], masterSkill[2])
+    for skill in skills:
+        if len(skill) == 3:
+            useHeroSkill(skill[0], skill[1], skill[2])
+        else:
+            useHeroSkill(skill[0], skill[1])
     card(hoguNo)
 
 
@@ -322,11 +345,10 @@ def doOneBossBattle():
 
 
 def reenter_battle():
-    sceneCheck.waitBackToMenu()
     # 确认已经返回菜单界面，或检测到连续出击按键
     flag, position = core.existPic('reenter_battle')
     if flag:
-        core.coreTouch(1040, 700, "连续出击")
+        core.coreTouch(1040, 650, "连续出击", 3)
 
 
 def apple_feed(appleType=0):
@@ -360,10 +382,8 @@ def quit_battle():
     #     if flag:
     #         rewardNum += 1
     #         core.log("目标奖励掉落+1，已掉落{}张".format(rewardNum))
-    core.coreTouch(986, 565, "不请求加好友", 5)
-    core.coreTouch(235, 525, "拒绝好友申请", 5)  # 拒绝好友申请
-
-    core.coreTouch(1376, 834, "离开结算界面", 5)  # 离开结算界面
+    
+    waitNextStep()
     global battleClearNum
     battleClearNum += 1
     logBattleEnd()
@@ -416,7 +436,6 @@ def existReward():
     flag, position = core.existPic('reward_pic')
     if flag:
         rewardNum += 1
-        mailSender.sendRewardDropMail(rewardNum)
 
 
 def battle_start():
@@ -445,11 +464,14 @@ def logQuitMsg():
     core.log('共通关 {} 次'.format(battleClearNum), level=2)
     if globalAppleType != 0:
         core.log('共用了 {} {}苹果,目标奖励掉落 {} 个'.format(appleUsedNum, appleTypes[globalAppleType][0], rewardNum), level=2)
-        mailSender.sendMail('脚本执行结束', '共通关 {} 次,共用了 {} {}苹果,目标奖励掉落 {} 个'.format(battleClearNum, appleUsedNum,
-                                                                                appleTypes[globalAppleType][0],
-                                                                                rewardNum))
 
-
+def waitNextStep():
+    flag = True
+    while flag:
+        flag, position = core.existPic('quit_battle')
+        if flag:
+            core.coreTouch(position[0], position[1], "点击下一步按钮")        
+        
 def start_FGO_process(times=1, appleType=0, servant=""):
     for i in range(0, times):
         find_friend(servant)
@@ -460,21 +482,40 @@ def start_FGO_process(times=1, appleType=0, servant=""):
         reenter_battle()
         apple_feed(appleType)
     quitWithMsg("")
+    
+def start_Fgo_FriendPoolGacha(times):
+    i = 0
+    while i < times:
+        flag, position = core.existPic('another_ten_gacha')
+        if flag:
+            #找到了继续十连抽按钮
+            core.coreTouch(position[0], position[1])
+            i = i + 1
+            core.coreSleep(1, "找到了10连按钮，点击")
+            confirmFlag = False
+            core.coreTouch(1150, 670)
+            core.coreSleep(2, "抽卡确认，点击")
+        else:
+            core.coreTouch(100, 500)
+            core.coreTouch(100, 500)
+            core.coreSleep(2, "没找到，抽卡中")
 
+
+def start_Fgo_InfinitePoolGacha(times):
+    i = 0;
+    while i < times:
+        core.coreTouch(500, 404)
+        core.coreTouch(500, 404)
+        core.coreSleep(1, "点击")
 
 # core.existPic('reenter_battle')
 # doOneBattle()
-start_FGO_process(30, 2, "CBA")
-# doOneRoundByConfig(round1)
-# doOneRoundByConfig(round3)
-# quit_battle()
-# core.existPic(pic = Template(r"tpl1610812989049.png", record_pos=(0.002, 0.206), resolution=(1581, 889)))
-# core.existPic(pic = Template(r"tpl1610814182310.png", record_pos=(0.237, -0.017), resolution=(1581, 889)))
-# core.existPic(pic = Template(r"tpl1610814359537.png", record_pos=(0.237, 0.136), resolution=(1581, 889)))
-# core.existPic(pic = Template(r"tpl1610815532809.png", record_pos=(0.154, 0.158), resolution=(1581, 889)))
-# reenter_battle()
-
-# find_friend("CBA")
-
-#     # Turn3
-# doOneRoundByConfig(round3)
+#connect_device("Android://127.0.0.1:5037")
+connect_device('Android://127.0.0.1:5037/emulator-5554') 
+#core.coreTouch(1033, 754)
+#自动战斗，需要到选择助战界面
+start_FGO_process(100, 1, "caber")
+# 友情池抽卡，需要抽一次十连出现继续十连
+#start_Fgo_FriendPoolGacha(100)
+# 无限池抽卡
+#start_Fgo_InfinitePoolGacha(300)
